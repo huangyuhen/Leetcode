84443ed68cce4d92bfd1a52c9fbe048633f6b26f
huangyuhen <huangyuhen@Hotmail.com>
2016-05-17 22:52:58 -0400

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.py
+++ 5-Longest-Palindromic-Substring/solution.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        ansl, ansr, maxx = 0, 1, 0
-        length = len(s)
-        for i in range(1, length * 2):
-            if i & 1 :
-                left = i / 2
-                right = left
-            else :
-                left = i / 2 - 1
-                right = left + 1
-            while (left >= 0) and (right < length) and (s[left] == s[right]):
-                left -= 1
-                right += 1
-            left += 1
-            right -= 1
-            if right - left > maxx:
-                maxx = right - left
-                ansl = left
-                ansr = right
-        return s[ansl: ansr + 1]
+        i = 0
+        maxl = [1, 0, 1]
+        while i + maxl[0]/2 < len(s):
+            start, end = i, i
+            while end + 1 < len(s) and s[start] == s[end+1]:
+                end += 1
+            i = end+1
+            while start - 1 >= 0 and end + 1 < len(s) and s[start - 1] == s[end + 1]:
+                start -= 1
+                end += 1
+            if (end - start) + 1 > maxl[0]:
+                maxl = [end - start+1, start, end+1]
+                continue
+        return s[maxl[1]: maxl[2]]
