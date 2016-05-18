0443b52c5dfbcfe3c8656196bea7519f6d243e63
huangyuhen <huangyuhen@Hotmail.com>
2016-05-18 18:17:42 -0400

    [53. Maximum Subarray][Accepted]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.py
+++ 53-Maximum-Subarray/solution.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        global_max, local_max = nums[0], nums[0]
-        for x in xrange(1, len(nums)):
-            local_max = max(nums[x], local_max + nums[x])
-            global_max = max(global_max, local_max)
-        return global_max
+        for i in xrange(1,len(nums)):
+                nums[i] = max(nums[i - 1] + nums[i], nums[i])
+        return max(nums)
