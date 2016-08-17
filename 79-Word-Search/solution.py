c8bccde2c0b67e251af26cf119169a33df59cb05
huangyuhen <huangyuhen@Hotmail.com>
2016-08-17 17:43:21 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.py
+++ 79-Word-Search/solution.py
@@ -14,10 +14,10 @@ class Solution(object):
                 return False
             if k + 1 == L:
                 return True
-            board[i][j] += '@'
+            temp, board[i][j] = board[i][j], '#'
 
             res = (i - 1 >= 0 and dfs(i - 1, j, k + 1)) or (i + 1 < M and dfs(i + 1, j, k + 1)) or (j - 1 >= 0 and dfs(i, j - 1, k + 1)) or (j + 1 < N and dfs(i, j + 1, k + 1))
-            board[i][j] = board[i][j][0]
+            board[i][j] = temp
             return res
         def DFS(i, j, l):
             if board[i][j] != word[l]: return False
