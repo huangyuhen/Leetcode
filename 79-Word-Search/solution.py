a7b2913f26d6771263c2bfa7ad3a381738c45e8c
huangyuhen <huangyuhen@Hotmail.com>
2016-08-17 17:42:54 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.py
+++ 79-Word-Search/solution.py
@@ -9,16 +9,16 @@ class Solution(object):
         L= len(word)
         M = len(board)
         N = len(board[0])
-        # def dfs(l, i, j):
-            # if word[k] != board[i][j]:
-            #     return False
-            # if k + 1 == len(word):
-            #     return True
-            # board[i][j] += '@'
+        def dfs(i, j, k):
+            if word[k] != board[i][j]:
+                return False
+            if k + 1 == L:
+                return True
+            board[i][j] += '@'
 
-            # res = (i - 1 >= 0 and dfs(k + 1, i - 1, j)) or (i + 1 <= len(board[0]) and dfs(k + 1, i + 1, j)) or (j - 1 >= 0 and dfs(k + 1, i, j - 1)) or (j + 1 <= len(board) and dfs(k + 1, i, j + 1))
-            # board[i][j] = board[i][j][0]
-            # return res
+            res = (i - 1 >= 0 and dfs(i - 1, j, k + 1)) or (i + 1 < M and dfs(i + 1, j, k + 1)) or (j - 1 >= 0 and dfs(i, j - 1, k + 1)) or (j + 1 < N and dfs(i, j + 1, k + 1))
+            board[i][j] = board[i][j][0]
+            return res
         def DFS(i, j, l):
             if board[i][j] != word[l]: return False
             if l+1 == L: return True
@@ -33,7 +33,7 @@ class Solution(object):
 
         for i in xrange(len(board)):
             for j in xrange(len(board[0])):
-                if DFS(i, j, 0):
+                if dfs(i, j, 0):
                     return True
         return False
 
