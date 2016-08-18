e2aaea341f3f2a92cd746230e9379b305eeb747b
huangyuhen <huangyuhen@Hotmail.com>
2016-08-18 17:35:45 -0400

    [47. Permutations II][Accepted]committed by LeetCode Extension

--- 47-Permutations-II/solution.py
+++ 47-Permutations-II/solution.py
@@ -4,15 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        length = len(nums)
-        if length == 0: return []
-        if length == 1: return [nums]
         nums.sort()
-        res = []
-        previousNum = None
-        for i in range(length):
-            if nums[i] == previousNum: continue
-            previousNum = nums[i]
-            for j in self.permuteUnique(nums[:i] + nums[i+1:]):
-                res.append([nums[i]] + j)
-        return res
+        result = []
+        used = [False] * len(nums)
+        self.permuteUniqueRecu(result, used, [], nums)
+        return result
+    
+    def permuteUniqueRecu(self, result, used, cur, nums):
+        if len(cur) == len(nums):
+            result.append(cur + [])
+            return
+        for i in xrange(len(nums)):
+            if used[i] or (i > 0 and nums[i-1] == nums[i] and not used[i-1]):
+                continue
+            used[i] = True
+            cur.append(nums[i])
+            self.permuteUniqueRecu(result, used, cur, nums)
+            cur.pop()
+            used[i] = False
